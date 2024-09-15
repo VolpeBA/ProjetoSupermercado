d908ef210890bbc7b31b81f5cf31084710adeb78
Bruno Volpe <bruno.volpe2015@gmail.com>
2024-09-15 19:10:55 -0300

    Criando a funcao useCarrinho

---
+++ ecommerce-supermarket/src/data/hooks/useCarrinho.ts
@@ -0,0 +1,5 @@
+import { useContext } from "react";
+import ContextoCarrinho from "../contexts/ContextoCarrinho";
+
+const UseCarrinho = () => useContext(ContextoCarrinho)
+export default UseCarrinho
