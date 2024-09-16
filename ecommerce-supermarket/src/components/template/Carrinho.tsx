73ca44c45d200d92ec0efcc2ce7278957c6788d0
Bruno Volpe <bruno.volpe2015@gmail.com>
2024-09-16 00:04:59 -0300

    Adicionando Local Storage

--- ecommerce-supermarket/src/components/template/Carrinho.tsx
+++ ecommerce-supermarket/src/components/template/Carrinho.tsx
@@ -3,14 +3,14 @@ import { IconShoppingCart } from '@tabler/icons-react';
 import Link from 'next/link';
 
 export default function Carrinho() {
-    const {quantidadeDeItens} = UseCarrinho()
+    const {qtdeDeItens} = UseCarrinho()
     return (
         <Link href="/carrinho">
             <div className='flex items-center relative'>
                 <IconShoppingCart size={32} stroke={1} />
                 <div className='absolute -top-2 -right-2 w-5 h-5 bg-red-500 rounded-full flex 
                 justify-center items-center text-xs'>
-                    {quantidadeDeItens}
+                    {qtdeDeItens}
                 </div>
             </div>    
         </Link>
--- ecommerce-supermarket/src/data/contexts/ContextoCarrinho.tsx
+++ ecommerce-supermarket/src/data/contexts/ContextoCarrinho.tsx
@@ -1,53 +1,65 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-'use client'
-import { createContext, useState } from "react";
-import Produto from "../model/produto";
-import ItemCarrinho from "../model/ItemCarrinho";
+import { createContext, useEffect, useState } from 'react'
+import ItemCarrinho from '../model/ItemCarrinho'
+import Produto from '../model/produto'
+import useLocalStorage from '../hooks/useLocalStorage'
 
 interface ContextoCarrinhoProps {
     itens: ItemCarrinho[]
-    quantidadeDeItens: number
+    qtdeDeItens: number
     adicionar: (item: Produto) => void
     remover: (item: Produto) => void
 }
 
-const ContextoCarrinho = createContext<ContextoCarrinhoProps>({} as any);
+const ContextoCarrinho = createContext<ContextoCarrinhoProps>({} as any)
 
 export function ProvedorCarrinho(props: any) {
     const [itens, setItens] = useState<ItemCarrinho[]>([])
+    const { set, get } = useLocalStorage()
+
+    useEffect(() => {
+        const carrinho = get('carrinho')
+        if (carrinho) {
+            setItens(carrinho)
+        }
+    }, [get])
 
     function adicionar(produto: Produto) {
         const indice = itens.findIndex((i) => i.produto.id === produto.id)
+
         if (indice === -1) {
-            setItens([...itens, {produto, quantidade: 1}])
+            alterarItens([...itens, { produto, quantidade: 1 }])
         } else {
             const novosItens = [...itens]
             novosItens[indice].quantidade++
-            setItens(novosItens)
+            alterarItens(novosItens)
         }
     }
 
     function remover(produto: Produto) {
-        const novosItens = itens.map((i) => {
-            if (i.produto.id === produto.id) {
-                i.quantidade--
-            }
-            return i
-        }).filter((i) => i.quantidade > 0)
-        setItens(novosItens)
+        const novosItens = itens
+            .map((i) => {
+                if (i.produto.id === produto.id) {
+                    i.quantidade--
+                }
+                return i
+            })
+            .filter((i) => i.quantidade > 0)
+        alterarItens(novosItens)
     }
 
-
+    function alterarItens(novosItens: ItemCarrinho[]) {
+        setItens(novosItens)
+        set('carrinho', novosItens)
+    }
 
     return (
-        <ContextoCarrinho.Provider 
+        <ContextoCarrinho.Provider
             value={{
                 itens,
                 adicionar,
                 remover,
-                get quantidadeDeItens() {
-                    return itens.reduce((acc, item) => acc + item.quantidade, 0)
+                get qtdeDeItens() {
+                    return itens.reduce((total, item) => total + item.quantidade, 0)
                 },
             }}
         >
